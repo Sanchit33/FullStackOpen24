f0623d61ea82be27943312f5ebb8e33032415ed2
Sanchit33 <96529417+Sanchit33@users.noreply.github.com>
2024-05-05 10:22:38 +0530

    Course Information, Step 5

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,18 +1,14 @@
 // Header Component
 const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
+  return <h1>{props.course}</h1>;
 };
 
 // Part Component
 const Part = (props) => {
+  const part = props.parts;
   return (
     <p>
-      {props.part}
-      {props.exercises}
+      {part.name} {part.exercises}
     </p>
   );
 };
@@ -21,57 +17,50 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      {props.parts.map((item) => (
+        <Part parts={item} />
+      ))}
     </>
   );
 };
 
 //Total Component
 const Total = (props) => {
+  const list = props.parts;
+  let total = 0;
+  list.forEach((item) => {
+    total += item.exercises;
+  });
   return (
     <>
-      <p>
-        Number of exercises{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
+      <p>Number of exercises {total}</p>
     </>
   );
 };
 
 function App() {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 }
