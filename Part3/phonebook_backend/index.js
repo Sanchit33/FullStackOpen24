ed85976381b890ccccef52aa0da275d1cbf7fbeb
Sanchit33 <96529417+Sanchit33@users.noreply.github.com>
2024-10-01 22:01:03 +0530

    Phonebook backend step 10

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -84,6 +84,6 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
