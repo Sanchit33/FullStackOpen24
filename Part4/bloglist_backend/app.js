5780167f09bb16e26d62e4f71f3389b08c4111f3
Sanchit33 <96529417+Sanchit33@users.noreply.github.com>
2024-12-13 21:16:35 +0530

    4.1 to 4.2 Blog List Step 1 and 2

---
+++ Part4/bloglist_backend/app.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controller/blogs')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+.then(() =>{
+    logger.info('connected to MongoDB')
+})
+.catch((error) => {
+    logger.error('error connecting to MongoBD:', error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+app.use(middleware.errorHandler)
+
+module.exports = app
+
+
---
+++ Part4/bloglist_backend/controller/blogs.js
@@ -0,0 +1,19 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (req, res) => {
+    Blog.find({}).then(blogs => {
+        res.json(blogs)
+    })
+})
+
+blogRouter.post('/', (req, res, next) => {
+    const blog = new Blog(req.body)
+
+    blog.
+    save().then(result => {
+        res.status(201).json(result)
+    }).catch(error => next(error))
+})
+
+module.exports = blogRouter
