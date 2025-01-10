3cb0e75a1f7ecc7ddda03a23f7ce8edda98ecbaf
Sanchit33 <96529417+Sanchit33@users.noreply.github.com>
2025-01-10 23:18:44 +0530

    4.10 Blog List Test Step 3

--- Part4/bloglist_backend/tests/blog_api.test.js
+++ Part4/bloglist_backend/tests/blog_api.test.js
@@ -4,17 +4,18 @@ const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
 const Blog = require('../models/blog')
-const hepler = require('./test_helper')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
 // this is not the final version need to change this in future
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(hepler.intialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(hepler.intialBlogs[1])
-    await blogObject.save()
+
+    const blogObject = helper.intialBlogs
+    .map(blog => new Blog(blog))
+
+    await Promise.all(blogObject.map(blog => blog.save()))
 })
 
 test('blogs are retured as json', async() => {
@@ -27,7 +28,7 @@ test('blogs are retured as json', async() => {
 test('all blogs are retured', async() => {
     const res = await api.get('/api/blogs')
 
-    assert.strictEqual(res.body.length, 2)
+    assert.strictEqual(res.body.length, helper.intialBlogs.length)
 })
 
 test('the first blog is about React patterns', async() => {
@@ -39,9 +40,32 @@ test('the first blog is about React patterns', async() => {
 })
 
 test('the blog post has unique identifier property named as id', async() => {
-    const res = await hepler.blogsInDb()
+    const res = await helper.blogsInDb()
 
     assert("id" in res[0], true)
+})
+
+test('valid blog can be added', async() => {
+    const newBlog = {
+        title: "React Native Pattern",
+        author: "Sanchit Shinde",
+        url: "https://reactpatterns.com/",
+        likes: 3,
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const res = await api.get('/api/blogs')
+
+    const contents = res.body.map(blog => blog.title)
+
+    assert.strictEqual(res.body.length, helper.intialBlogs.length + 1)
+    assert(contents.includes('React Native Pattern'), true)
+
 })
 
 after(async () => {
