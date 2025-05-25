ce228959e01002b1e1800ae8b6b1cf947713ac0a
Sanchit33 <96529417+Sanchit33@users.noreply.github.com>
2025-05-25 10:10:28 +0530

    5.14: Blog List Tests, step 2

--- Part5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
+import { userEvent } from "@testing-library/user-event";
 
 test(" blog renders the blog's title and author, but does not render its URL or number of likes by default", () => {
   const blog = {
@@ -26,4 +27,31 @@ test(" blog renders the blog's title and author, but does not render its URL or 
   expect(element2).toBeDefined();
   expect(element3).toBeNull();
   expect(element4).toBeNull();
+});
+
+test("clicking the button shows the blog's URL and number of likes", async () => {
+  const blog = {
+    title: "Test Blog",
+    author: "Test Author",
+    url: "http://testurl.com",
+    likes: 5,
+    user: {
+      username: "test username",
+      name: "test name",
+    },
+  };
+
+  render(<Blog key={blog.id} blog={blog} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  screen.debug();
+
+  const element3 = screen.queryByText("http://testurl.com");
+  const element4 = screen.queryByText("5 likes");
+
+  expect(element3).toBeDefined();
+  expect(element4).toBeDefined();
 });
